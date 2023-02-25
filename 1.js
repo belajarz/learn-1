8cfb5e361f0abaf183cb866f91d01a05da60ec6a
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-02-25 10:28:06 +0700

    init

---
+++ 1.js
@@ -0,0 +1,6 @@
+//1. buatlah sebuah array bernama vehicles dengan isi 5 data dengan value string 'NAMA NAMA KENDARAAN'
+//2. Tampilkan vehices didalam terminal dengan console log
+//3. Tampilkan satu persatu nama nama kendaraan tersebut dengan metode for loop
+//4. Ganti salah satu value dari array tersebut dengan nama kendaraan yang lain
+//5. Tampilkan salah satu dari isi array tersebut sebanyak 3 kali
+//6. buatlah sebuah function yang dapat mengurangi 2 buah angka
